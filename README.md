8c76c6688d3a8adcb437d5a382b2e64af3c24031
David Dollar <ddollar@gmail.com>
2010-11-18 13:33:16 -0500

    pgbackups can autoexpire

--- README.md
+++ README.md
@@ -26,16 +26,13 @@ Make sure you install the pgbackups addon
 
 ## Configuration
 
-By default, `heroku_backup_task` will back up `DATABASE_URL` and will keep
-a maximum of 2 backups. You can change these defaults:
+By default, `heroku_backup_task` will back up `DATABASE_URL`. You can change this with:
 
     heroku config:add HEROKU_BACKUP_DATABASES="SOME_DATABASE_URL,OTHER_DATABASE_URL"
-    heroku config:add HEROKU_BACKUP_MAXBACKUPS=7
     
-Make sure that `HEROKU_BACKUP_MAXBACKUPS` is less than or equal to the number
-of backups supported by your pgbackups plan.
-
 ## Usage
+
+### NOTE: `heroku_backup_task` will expire your oldest backup to make room for a new backup if necessary.
 
 Set up cron on your application with
 
@@ -44,8 +41,6 @@ Set up cron on your application with
 You will see something like this in your cron logs
 
     [Thu Nov 18 12:59:56 -0500 2010] starting heroku backup task
-    [Thu Nov 18 12:59:56 -0500 2010] too many backups (2 max), purging 1 to make room
-    [Thu Nov 18 12:59:56 -0500 2010]   removing b007
     [Thu Nov 18 12:59:57 -0500 2010] backing up: DATABASE_URL
 
 ## License
--- lib/heroku_backup_task.rb
+++ lib/heroku_backup_task.rb
@@ -23,44 +23,19 @@ module HerokuBackupTask; class << self
     end
   end
 
-  def max_backups
-    if max = ENV["HEROKU_BACKUP_MAXBACKUPS"]
-      max.to_i
-    else
-      2
-    end
-  end
-
   def backup_name(to_url)
     # translate s3://bucket/email/foo/bar.dump => foo/bar
     parts = to_url.split('/')
     parts.slice(4..-1).join('/').gsub(/\.dump$/, '')
   end
 
-  def make_room_for_new_backup
-    backups = client.get_transfers.sort_by { |t| t["created_at"] }
-    num_to_remove = backups.length - max_backups + databases.length
-
-    if num_to_remove > 0
-      log "too many backups (#{max_backups} max), purging #{num_to_remove} to make room"
-
-      backups[0,num_to_remove].each do |backup|
-        name = backup_name(backup["to_url"])
-        log "  removing #{name}"
-        client.delete_backup(name)
-      end
-    end
-  end
-
   def execute
     log "starting heroku backup task"
-
-    make_room_for_new_backup
 
     databases.each do |db|
       db_url = ENV[db]
       log "backing up: #{db}"
-      client.create_transfer(db_url, db, nil, "BACKUP")
+      client.create_transfer(db_url, db, nil, "BACKUP", :expire => true)
     end
   end
 
